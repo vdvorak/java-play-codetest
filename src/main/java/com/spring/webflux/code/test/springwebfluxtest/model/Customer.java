cfa0c6c4079b441befd81b41c7940bac04e8e848
vdvorak <viktor.dvorak83@gmail.com>
2018-03-12 03:52:54 +0200

    create @RestConntroller

--- src/main/java/com/spring/webflux/code/test/springwebfluxtest/model/Customer.java
+++ src/main/java/com/spring/webflux/code/test/springwebfluxtest/model/Customer.java
@@ -6,11 +6,75 @@ import org.joda.time.DateTime;
 /**
  * Created by vdvorak on 11.03.18.
  */
+
 public class Customer {
     private Integer id;
     private String name;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-ddTHH:mm:ss.SSSZ")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     private DateTime duetime;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-ddTHH:mm:ss.SSSZ")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     private DateTime jointime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DateTime getDuetime() {
+        return duetime;
+    }
+
+    public void setDuetime(DateTime duetime) {
+        this.duetime = duetime;
+    }
+
+    public DateTime getJointime() {
+        return jointime;
+    }
+
+    public void setJointime(DateTime jointime) {
+        this.jointime = jointime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+
+        Customer customer = (Customer) o;
+
+        if (!duetime.equals(customer.duetime)) return false;
+        if (!id.equals(customer.id)) return false;
+        if (!jointime.equals(customer.jointime)) return false;
+        if (!name.equals(customer.name)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", duetime=" + duetime +
+                ", jointime=" + jointime +
+                '}';
+    }
 }
--- src/main/java/com/spring/webflux/code/test/springwebfluxtest/view/View.java
+++ src/main/java/com/spring/webflux/code/test/springwebfluxtest/view/View.java
@@ -1,13 +1,19 @@
 package com.spring.webflux.code.test.springwebfluxtest.view;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.spring.webflux.code.test.springwebfluxtest.model.Customer;
+import org.joda.time.DateTime;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by vdvorak on 11.03.18.
@@ -15,12 +21,25 @@ import java.util.List;
 @RestController
 public class View {
 
-    @GetMapping("/index")
-    public Flux<Integer> index() {
-        List<Integer> rt = new ArrayList<Integer>(Arrays.asList(11,2,1,8));
-        return Flux.fromIterable(rt).sort();
+    private final Logger LOGGER = Logger.getLogger(this.getClass().getName());
 
+    @PostMapping(value = "/sort/customer/by/dueTime", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_EVENT_STREAM_VALUE})
+    public ResponseEntity<?> sortCustomerByDueTime(@RequestBody List<Customer> listCustomer) {
+        listCustomer.forEach((s) -> {
+            System.out.println(s);
+        });
+        // sort by duetime
+        Comparator<Customer> byDueTime = (Customer o1, Customer o2) -> o1.getDuetime().compareTo(o2.getDuetime());
+        return ResponseEntity.ok(Flux.fromIterable(listCustomer).sort(byDueTime));
     }
-    
+
+    @PostMapping(value = "/sort/customer/by/joinTime", produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
+    public ResponseEntity<?> sortCustomerByJoinTime(@RequestBody final List<Customer> listCustomer) {
+        Comparator<Customer> byJoinTime = (Customer o1, Customer o2) -> o1.getJointime().compareTo(o2.getJointime());
+        listCustomer.forEach((s) -> {
+            System.out.println(s);
+        });
+        return ResponseEntity.ok(Flux.fromIterable(listCustomer).sort(byJoinTime));
+    }
 
 }
